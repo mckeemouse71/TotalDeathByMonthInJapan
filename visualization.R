73d3dae63c3b8a1625b5e5a63fb7f203736a29d8
mckeemouse71 <akiyuki.honda@gmail.com>
2021-10-25 23:06:51 +0900

    Add visualization result

--- visualization.R
+++ visualization.R
@@ -9,5 +9,7 @@ data_p <- data
 data_p$Year <- as.factor(data_p$Year)
 
 fig <- plot_ly(data_p, x=~Month, y=~Death, color=~Year,
-               mode = 'lines+markers')
+               mode = 'lines+markers') %>% 
+        layout(title="Total Death by Month in Japan",
+               legend=list(traceorder="reversed"))
 fig
